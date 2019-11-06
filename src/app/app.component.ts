ff26739d4ea3a46616aa0217d2a9b513e5a587ef
Victor Hernandez <victorhernandez@Victors-MacBook-Pro.local>
2019-11-05 22:48:35 -0500

    initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {Product} from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'victor' ];
+  products: Product = [
+    {
+      id: '1',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+      image: 'assets/images/camiseta.png'
+    },
+    {
+      id: '2',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+      image: 'assets/images/hoodie.png'
+    },
+    {
+      id: '3',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+      image: 'assets/images/mug.png'
+    },
+    {
+      id: '4',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+      image: 'assets/images/pin.png'
+    },
+    {
+      id: '5',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+      image: 'assets/images/stickers1.png'
+    },
+    {
+      id: '6',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+      image: 'assets/images/stickers2.png'
+    },
+  ];
+  addItem() {
+    this.items.push('new Item');
+  }
+
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
